1e381076c7a85a41aba1d6455c4822c3fe4733a8
Adam Zekiec <adam.zekiec@gmail.com>
2026-05-20 16:39:33 +0200

    Add Inspiration UI

--- app/src/main/java/com/example/corda/ui/CordaApp.kt
+++ app/src/main/java/com/example/corda/ui/CordaApp.kt
@@ -8,9 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
-import com.example.corda.ui.util.findComponentActivity
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
